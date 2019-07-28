843c173f751e5c30cacf4333ae712b89fdc87050
Mukul Khanna <mukulkhanna.96@gmail.com>
2019-07-28 22:13:07 +0530

    First commit, new creation

---
+++ blogpost/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+from django.utils import timezone
+from django.shortcuts import reverse
+
+# Create your models here.
+
+
+class Post(models.Model):
+    """
+    Post model for blog.
+    """
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True,null=True)
+
+    def publish(self):
+        """
+        method to publish the post
+        """
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        """
+        method to approve comments
+        :return: only approved comments
+        """
+        return self.comments.filter(approved_comment=True)
+
+    def get_absolute_url(self):
+        return reverse("post_detail",kwargs={"pk":self.pk})
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    """
+    class for Comment Model
+    """
+    post = models.ForeignKey('Post',related_name='comments',on_delete=models.CASCADE)
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        """
+        approve comments
+        """
+        self.approved_comment = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse("post_list")
+
+    def __str__(self):
+        return self.text
